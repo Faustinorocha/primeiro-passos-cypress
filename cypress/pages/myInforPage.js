6204bfda4e132ef3e8a61d21c2a8a13fa2f260ec
Faustino Rocha <faustinorocha1@gmail.com>
2025-05-13 15:35:57 -0300

    Criação dos pages objects

--- cypress/pages/myInforPage.js
+++ cypress/pages/myInforPage.js
@@ -9,7 +9,7 @@ class MyInfoPage {
             submitButton: "[type='submit']",
             comboboxnationalityselection: ':nth-child(5) > :nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input',
             comboboxmaritalselection: ":nth-child(5) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input",
-            nationalityselection: ":nth-child(4) > span",
+            nationalityselection: ":nth-child(3) > span",
             maritalselection: ".oxd-select-dropdown > :nth-child(2) > span"
 
             
@@ -29,7 +29,7 @@ class MyInfoPage {
       cy.get(this.selectorsListInfo().middleNameFied).clear().type("silva")
       cy.get(this.selectorsListInfo().lastNameField).clear().type("bezerra")
       cy.get(this.selectorsListInfo().comboboxnationalityselection).click()
-      //cy.get(this.selectorsListInfo().nationalityselection).eq(1).click(this.selectorsListInfo.nationalityselection)
+      cy.get(this.selectorsListInfo().nationalityselection).click(this.selectorsListInfo.nationalityselection)
       cy.get(this.selectorsListInfo().comboboxmaritalselection).click()
       cy.get(this.selectorsListInfo().maritalselection).click(this.selectorsListInfo.maritalselection)
       cy.get(this.selectorsListInfo().submitButton).eq(0).click()
